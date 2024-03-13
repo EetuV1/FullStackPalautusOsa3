ce1a4484492413cd16831ec1f437abff73dcdf12
Eetu Valkamo <eetu.valkamo@edu.turkuamk.fi>
2024-03-13 14:15:29 +0200

    3.11 valmis

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Fly.io:
+Fly.io linkki sovellukseen:
 https://osa3.fly.dev
 
+Deploy komento:
 fly deploy
+
+Näytä logi komento:
+fly logs
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
-const app = express()
 const morgan = require("morgan")
 const cors = require("cors")
 
+const app = express()
+
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(cors())
 
